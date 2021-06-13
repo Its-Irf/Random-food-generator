a044ed41abc2bede3e8d7f1852789332391c9ea6
Its-Irf <83921080+Its-Irf@users.noreply.github.com>
2021-06-13 16:15:17 -0400

    Create foodRunner.java

---
+++ foodRunner.java
@@ -0,0 +1,10 @@
+
+public class foodRunner {
+    public static void main(String[] args) {
+        foodList aFoodList=new foodList();
+        aFoodList.getFoodList("food_list");
+    }
+        
+}
+    
+
