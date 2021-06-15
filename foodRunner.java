4613018e7ad70b924eb61ccf9295202e52b36590
Its-Irf <83921080+Its-Irf@users.noreply.github.com>
2021-06-14 20:08:45 -0400

    Update foodRunner.java
    Added method to add predetermined amount of food items to preexisting arraylist(in the future make it so that it can make a new txt file with data, and adds check in feature to ensure no mispelt words or phrases)

--- foodRunner.java
+++ foodRunner.java
@@ -2,7 +2,9 @@
 public class foodRunner {
     public static void main(String[] args) {
         foodList aFoodList=new foodList();
-        aFoodList.getFoodList("food_list");
+        aFoodList.loadFoodList("food_list");
+      //  aFoodList.add(new Food());
+        aFoodList.addFoodtoList();
     }
         
 }
